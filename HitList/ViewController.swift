d869625d8127643040105b1a05499d832270ed1b
Andres Arana <3av.arana@gmail.com>
2023-06-01 17:55:31 -0400

    Create Person model in Core Data Model File

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -6,13 +6,13 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var names: [String] = []
-
+    var people: [NSManagedObject] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,18 +29,18 @@ class ViewController: UIViewController {
                                     message: "Add a new name",
                                     preferredStyle: .alert)
       
-      let saveAction = UIAlertAction(title: "Save",
-                                     style: .default) {
-        [unowned self] action in
-                                      
-        guard let textField = alert.textFields?.first,
-          let nameToSave = textField.text else {
-            return
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+          [unowned self] action in
+          
+          guard let textField = alert.textFields?.first,
+            let nameToSave = textField.text else {
+              return
+          }
+          
+          self.save(name: nameToSave)
+          self.tableView.reloadData()
         }
-        
-        self.names.append(nameToSave)
-        self.tableView.reloadData()
-      }
+
       
       let cancelAction = UIAlertAction(title: "Cancel",
                                        style: .cancel)
@@ -53,21 +53,57 @@ class ViewController: UIViewController {
       present(alert, animated: true)
     }
 
-
+    func save(name: String) {
+      
+      guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+        return
+      }
+      
+      // 1
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+      
+      // 2
+      let entity =
+        NSEntityDescription.entity(forEntityName: "Person",
+                                   in: managedContext)!
+      
+      let person = NSManagedObject(entity: entity,
+                                   insertInto: managedContext)
+      
+      // 3
+      person.setValue(name, forKeyPath: "name")
+      
+      // 4
+      do {
+        try managedContext.save()
+        people.append(person)
+      } catch let error as NSError {
+        print("Could not save. \(error), \(error.userInfo)")
+      }
+    }
 }
+
 
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView,
+  func tableView(_ tableView: UITableView,
                  numberOfRowsInSection section: Int) -> Int {
-        return names.count
-    }
-  
-    func tableView(_ tableView: UITableView,
+    return people.count
+  }
+
+  func tableView(_ tableView: UITableView,
                  cellForRowAt indexPath: IndexPath)
                  -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = names[indexPath.row]
-                     return cell
-    }
+
+    let person = people[indexPath.row]
+    let cell =
+      tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                    for: indexPath)
+    cell.textLabel?.text =
+      person.value(forKeyPath: "name") as? String
+    return cell
+  }
 }
+
